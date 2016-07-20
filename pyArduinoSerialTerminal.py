ffd848e523245dc22d8b945c9bbe29e2b0c503c6
James Aguirre <jaguirre@sas.upenn.edu>
2016-07-20 17:36:25 -0400

    pyArduinoSerialTerminal.py basically reproduces the Arduino Serial Monitor functionality now, though there is a bit of an offset between when the Arduino sends something to the buffer and this program reads it

--- pyArduinoSerialTerminal.py
+++ pyArduinoSerialTerminal.py
@@ -1,7 +1,7 @@
 import serial
 import arduinoserial
 
-port='/dev/cu.usbmodem1451'
+port='/dev/cu.usbmodem1411'
 baud = 115200 # 9600
 #EOT = 'XXX\r\n'
 EOT = '\r\n'
@@ -28,18 +28,25 @@ def read_ser_buffer_to_eot():
         output.append(buffer)
         buffer = ser.readline()
     return output
-    
+
+def print_ser_buffer():
+    while (ser.inWaiting() != 0):
+        print ser.readline()
+    return
+
 while(operate):
-    if (ser.inWaiting() != 0):
-        print_ser_buffer_to_eot() # Clear out the buffer
+    #if (ser.inWaiting() != 0):
+    #    print_ser_buffer_to_eot() # Clear out the buffer
+    print_ser_buffer()
     var = raw_input("Enter command to transmit, Q to quit: ")
     if not var == 'Q':
         if var == 'C':
-            read_ser_buffert_to_eot()
+            #read_ser_buffert_to_eot()
+            print_ser_buffer()
         else:
             print "Sending "+var
             ser.write(var)
-            print_ser_buffer_to_eot()
+            #print_ser_buffer_to_eot()
     else:
         operate = False
 
