4561eb6d491151184a1c4cd451bf36ed52d51b66
James Aguirre <jaguirre@sas.upenn.edu>
2016-08-15 17:33:20 -0400

    added a test of handshaking

---
+++ Arduino/HandshakeSerial/HandshakeSerial.ino
@@ -0,0 +1,84 @@
+char user_input;
+float flt;
+
+#define ELEN  7  
+#define AZEN  10 
+
+#define BTX "BTX"
+#define ETX "ETX"
+#define RCVD 'R'
+#define EOT "ZZZ"
+
+int analogPin = 0;
+int x, val;
+
+//String user_input = String('initialize');
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ELEN, OUTPUT);
+  pinMode(AZEN, OUTPUT);
+  digitalWrite(ELEN, HIGH); 
+  digitalWrite(AZEN, HIGH); 
+  Serial.begin(115200);
+  //
+  Serial.println("Testing serial back and forth");
+  Serial.println(EOT);
+  
+}
+
+void loop() {
+  int i;
+  unsigned long sec0, sec;
+  
+  // put your main code here, to run repeatedly:
+  while(Serial.available()){
+      user_input = Serial.read();
+      if (user_input == 'n'){
+          //
+          Serial.println("Enter number of data points to transmit: ");
+          Serial.println(EOT);
+          while (Serial.available()==0){ }
+          flt = Serial.parseFloat();
+          for(x=1; x<int(flt); x++){
+            Serial.print("Sample ");
+            Serial.println(x);
+            Serial.print(" of ");
+            Serial.println(int(flt));
+            Handshake();
+          }
+          //
+          Serial.print("Printing input float: ");
+          Serial.println(flt);
+          Serial.println(EOT);
+      }  
+      else {
+          Serial.println("Invalid option");
+          Serial.println(EOT);
+      }   
+  }
+}
+
+void Handshake(){
+  char rcvd_val;
+  boolean received;
+  
+  Serial.println(BTX);
+  val = analogRead(analogPin);
+  Serial.println(val);
+  Serial.println(ETX);
+  received = false;
+  while(!received){
+    Serial.println("Waiting for verification");
+    while (Serial.available()==0){ }
+    rcvd_val = Serial.read();
+    Serial.println(rcvd_val);
+    Serial.println(received);
+    if (rcvd_val == 'R'){
+      Serial.println("Transmission verified");
+      received=true;
+      Serial.println(received);
+    }
+  }
+}
+
