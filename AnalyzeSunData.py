5176fc351a5bd142e1bf8bfd17b867ae78ef1b1d
James Aguirre <jaguirre@sas.upenn.edu>
2017-07-07 15:49:47 -0400

    Fri Jul  7 15:49:47 EDT 2017

---
+++ AnalyzeSunData.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  7 14:43:21 2017
+
+@author: jaguirre
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import ephem
+
+def readMRT(filename):
+    dat = np.load(filename)
+    d = {}
+    for item in dat.items():
+        d[item[0]] = item[1]
+#   Determine active axis
+    if (np.median(np.diff(d['az']))==0):
+        d['aa'] = 'el'
+    else:
+        d['aa'] = 'az'
+    return d
+
+filenames = glob('Fri_Jun_30_*.npz')
+for i,filename in enumerate(filenames):
+    d = readMRT(filename)
+    plt.figure(i)
+    plt.clf()
+    plt.plot(d[d['aa']],d['pwr'])
+    plt.title(filename)
+    plt.xlabel(d['aa']+' scan (deg)')
+    plt.ylabel(r'Power ($\mu$W)')
