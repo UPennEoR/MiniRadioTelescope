6d810a23111819efc094942d99c212b931d5f604
Oscar Tinney <otinney@sas.upenn.edu>
2018-07-03 13:34:30 -0400

    new file containing information on the state of the telescope

---
+++ mrtstate.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 13:59:11 2018
+
+@author: oscartinney
+"""
+
+state_vars = ['lastCMDvalid',
+              'elDeg',
+              'elSteps',
+              'azDeg',
+              'azSteps',
+              'axis',
+              'mode',
+              'sense',
+              'elEnable',
+              'azEnable',
+              'voltage',
+              'ax',
+              'ay',
+              'az',
+              'mx',
+              'my',
+              'mz',
+              'pitch',
+              'roll',
+              'heading']
+
+state_dtypes=['<U16', #'string',
+              'float64',
+              'int64',
+              'float64',
+              'int64',
+              '<U16', #'string',,
+              '<U16', #'string',
+              'int64',
+              'int',
+              'int',
+              'float64',
+              'float64',
+              'float64',
+              'float64',
+              'float64',
+              'float64',
+              'float64',
+              'float64',
+              'float64',
+              'float64']
+
+state = {}
+for state_var in state_vars:
+    state[state_var] = []
