8514fe9b4e7b251358f537f08193ef1fc49c09de
Oscar Tinney <otinney@sas.upenn.edu>
2018-07-09 16:29:02 -0400

    WE ARE SO CLOSE! Just need to work on the bugs in mapping functions to accomodate changes in how scan and goto and state work

--- MRT_FUNC_PY3.py
+++ MRT_FUNC_PY3.py
@@ -97,9 +97,8 @@ def numpyState(state):
     ndata['pwr'] = mrt.zx47_60(ndata['voltage'])
     # Both readState and readStream run through here.
     # Apply offsets
-    ndata['azDeg'] = np.mod(-ndata['azDeg'] - azoff,360)
-    ndata['elDeg'] -= eloff 
-
+    ndata['azDeg'] = np.round(np.mod(-ndata['azDeg'] - azoff,360),3)
+    ndata['elDeg'] = np.round(ndata['elDeg']-eloff, 3)
     return ndata
 
 def parseState(buffer,state):
@@ -110,7 +109,7 @@ def parseState(buffer,state):
     if len(vars) != len(mrtstate.state_vars):
         print('Cannot parse the returned state')
         FlushSerialBuffers(ser)
-        state = initState()
+        state = mrtstate.state #initState()
     else:
         for i,var in enumerate(vars):
             state[mrtstate.state_vars[i]].append(var)
@@ -128,51 +127,25 @@ def readState(ser,init=None):
     mrtstate.state = ndata
     return ndata
 
-'''
-def readState(ser,init=0):
-    # Initialize the dictionary, unless a previous state is passed in
-    if init == 0:
-        data = initState()
-    else:
-        data = init
-    buf = read_ser_buffer_to_eot(ser)
-    data = parseState(buf,data)
-    if data is not 0:
-        mrtstate.state = numpyState(data)
-    #else:
-    #    ndata = mrtstate.state #None
-    
-    return #ndata
-
-def readState(ser):
-    # Initialize the dictionary, unless a previous state is passed in
-    data = initState()
-    buf = read_ser_buffer_to_eot(ser)
-    data = parseState(buf,data)
-    if data is not None:
-        mrtstate.state = numpyState(data)
-    #else:
-    #    ndata = mrtstate.state #None
-    
-    return #ndata
-'''
 # GODDAMMIT!  There are two places where I read data: readState and readStream
 def readStream(ser):
     """ Generalize read_data to read an arbitrary list """
     data = initState()
     # Begin reading serial port
     buf = read_ser_buffer_to_eot(ser) #ser.readline()
-    #print '1 BUFFER', buf[0]
+    print('1 BUFFER', buf[0])
     # Read anything you see until you see BDTX
     while (buf[0] != BDTX):
         buf = read_ser_buffer_to_eot(ser) #ser.readline()
-        #print '2 BUFFER:', buf[0]
+        print('2 BUFFER:', buf[0])
     # Then read states
     while(buf[0] != EDTX):
         buf = read_ser_buffer_to_eot(ser)
         if (buf[0] != EDTX):
+            print(buf[0])
             data = parseState(buf, data)
     ndata = numpyState(data)
+    StdCmd(ser,REPORT_STATE)
     return ndata
 
 def StdCmd(ser,cmd):
@@ -195,10 +168,16 @@ def read_ser_buffer_to_eot(ser):
     return output
 
 def Scan(ser,deg):
+    print('Writing',SCAN)
     ser.write(SCAN)
-    ser.write(str.encode(deg))
+    # The round statement is necessary to prevent a problem with interpretation
+    # by the Arduino when converted to an ASCII string.
+    # Is it possible to send floats directly to the Arduino?
+    deg_str = str.encode(str(np.round(deg,3)))
+    print('Writing',deg_str)
+    ser.write(deg_str)
     data = readStream(ser)
-    mrtstate.state = readState(ser)
+    StdCmd(ser,REPORT_STATE)
     return data
 
 def PlotData(ndata):
@@ -227,51 +206,6 @@ def PlotData(ndata):
     plt.show()
     return
 
-'''
-def GoTo(current_state,azG=None,elG=None):
-    if azG == None:
-        azG = input("Az: ")
-        azG = float(azG)
-    if elG == None:
-        elG = input("El: ")
-        elG = float(elG)
-    
-    """ I just chose the wrong convention for azimuth rotation.  
-    I picked that F is CCW and increasing numbers, and the astronomical
-    convention would be that CW from above is increasing (N through E).
-    So we have to do a sign flip on the AZ and a counterintuitive 
-    direction for the motion.  Idiot.  """
-    # Should find a way to encode these limits elsewhere
-    if ((azG >=0. and azG <= 360.) and (elG >= -eloff and elG <= 120.)):
-        # Deltas are defined as desired minus current position
-        d_az = azG - float(current_state['azDeg'][0])
-        d_el = elG - float(current_state['elDeg'][0])
-        print ('d_az',d_az)
-        print ('d_el',d_el)
-        current_state = StdCmd(ser,AZIMUTH)
-        current_state = StdCmd(ser,ENABLE)
-        if d_az < 0:
-            # If moving to a less positive azimuth, go CCW
-            current_state = StdCmd(ser,FORWARD)
-        else:
-            # If moving to a more positive azimuth, go CW
-            current_state = StdCmd(ser,REVERSE)
-            print ('Starting from')
-            PrintState()
-            d,current_state = Scan(ser,str(np.abs(d_az)))
-            current_state = StdCmd(ser,ELEVATION)
-            current_state = StdCmd(ser,ENABLE)
-        if d_el < 0:
-            current_state = StdCmd(ser,REVERSE)
-        else:
-            current_state = StdCmd(ser,FORWARD)
-            d,current_state = Scan(ser,str(np.abs(d_el)))
-            print ('Ending at')
-            PrintState()
-    else:
-        print ('Requested az/el out of bounds')
-'''
-
 def GoTo(azG=None,elG=None):
     if azG == None:
         azG = input("Az: ")
@@ -283,143 +217,102 @@ def GoTo(azG=None,elG=None):
     d_el = elG - float(mrtstate.state['elDeg'][0])
     print ('d_az: ',d_az)
     print ('d_el: ',d_el)
-    "THIS IS A NEW ATTEMP"
     if (azG >=0. and azG <= 360.):
-        mrtstate.state = StdCmd(ser,AZIMUTH)
-        #print(mrtstate.state)
-        mrtstate.state = StdCmd(ser,ENABLE)
+        az_ok = True
+    else:
+        az_ok = False
+        print ('Requested azimuth out of bounds')
+    if (elG >= -eloff and elG <= 120.):
+        el_ok = True
+    else:
+        el_ok = False
+        print ('Requested elevation out of bounds')
+        
+    if (az_ok and el_ok):
+        # Do the azimuth move
+        StdCmd(ser,AZIMUTH)
+        print(mrtstate.state)
+        StdCmd(ser,ENABLE)
+        print(mrtstate.state)
+        print ('Azimuth move starting')
+        PrintState()
         if d_az < 0:
             # If moving to a less positive azimuth, go CCW
-            mrtstate.state = StdCmd(ser,FORWARD)
-            print ('Starting from')
-            PrintState()
-            #d = Scan(ser,str(np.abs(d_az)))
-            ser.write(SCAN(ser,str(np.abs(d_az))))
-            mrtstate.state = readState(ser)
-            azG=None
-            print('hi1')
+            StdCmd(ser,FORWARD)   
+            print(mrtstate.state)
         else:
-            # If moving to a more positive azimuth, go CW
-            mrtstate.state = StdCmd(ser,AZIMUTH)
-            print('hi1')
-            mrtstate.state = StdCmd(ser,REVERSE)
-            print('hi3')
-            print ('Starting from')
-            print('hi4')
-            PrintState()
-            print('hi5')
-            #d = Scan(ser,str(np.abs(d_az)))
-            SCAN(ser,(np.abs(d_az)))
-            #ser.write(str.encode(np.abs(d_az)))
-            print('hi6')
-            mrtstate.state = readState(ser)
-            print('hi7')
-            azG=None
-            print('hi8')
-    else:
-        print ('Requested az/el out of bounds')
-        azG=None
-        elG=None
-    if (elG >= -eloff and elG <= 120.):
-        mrtstate.state = StdCmd(ser,ELEVATION)
-        mrtstate.state = StdCmd(ser,ENABLE)
+            StdCmd(ser,REVERSE)
+            print(mrtstate.state)
+        print(str(np.abs(d_az)))
+        Scan(ser,np.abs(d_az))
+        print('Azimuth move ended at')
+        print(mrtstate.state)
+        # Elevation move
+        StdCmd(ser,ELEVATION)
+        StdCmd(ser,ENABLE)
+        print ('Elevation move')
+        PrintState()
         if d_el < 0:
             mrtstate.state = StdCmd(ser,REVERSE)
-            #d = Scan(ser,str(np.abs(d_el)))
-            ser.write(SCAN(ser,str(np.abs(d_el))))
-            mrtstate.state = readState(ser)
-            print ('Ending at')
-            PrintState()
-            elG=None
-            print('hi3')
         else:
             mrtstate.state = StdCmd(ser,FORWARD)
-            #d = Scan(ser,str(np.abs(d_el)))
-            ser.write(SCAN(ser,str(np.abs(d_el))))
-            mrtstate.state = readState(ser)
-            print ('Ending at')
-            PrintState()
-            elG=None
-            print('hi4')
-    else:
-        print ('Requested az/el out of bounds')
-        azG=None
-        elG=None
-    return #(current_state)
+        Scan(ser,np.abs(d_el))
+        print ('Final state')
+        PrintState()
+    return
 
-def GoAz(current_state,azGa=None):
+def GoAz(azGa=None):
     if azGa == None:
         azGa = input("Az: ")
         azGa = float(azGa)
-    d_azga = azGa - float(current_state['azDeg'][0])
+    d_azga = azGa - float(mrtstate.state['azDeg'][0])
     print ('d_az: ',d_azga)
     if (azGa >=0. and azGa <= 360.):
-        current_state = StdCmd(ser,AZIMUTH)
-        current_state = StdCmd(ser,ENABLE)
+        StdCmd(ser,AZIMUTH)
+        StdCmd(ser,ENABLE)
+        print ('Azimuth move starting')
+        PrintState()
         if d_azga < 0:
             # If moving to a less positive azimuth, go CCW
-            current_state = StdCmd(ser,FORWARD)
-            print ('Starting from')
-            PrintState()
-            Scan(ser,np.abs(d_azga))
-            #d,current_state = Scan(ser,str(np.abs(d_azga)))
-            #current_state = readState(ser)
-            print ('Ending at')
-            PrintState()
+            StdCmd(ser,FORWARD)   
         else:
-            # If moving to a more positive azimuth, go CW
-            current_state = StdCmd(ser,AZIMUTH)
-            current_state = StdCmd(ser,REVERSE)
-            print ('Starting from')
-            PrintState()
-            ser.write(SCAN)
-            deg = np.abs(d_azga)
-            ser.write(str.encode(deg))
-            #d,current_state = Scan(ser,str(np.abs(d_azga)))
-            #current_state = readState(ser)
-            print ('Ending at')
-            PrintState()
+            StdCmd(ser,REVERSE)
+        print(str(np.abs(d_azga)))
+        Scan(ser,np.abs(d_azga))
+        print('Azimuth move ended at')
+        PrintState()
+        print ('Final state')
+        PrintState()
     else:
-        print ('Requested az out of bounds')
+        print ('Requested azimuth out of bounds')
         azGa=None
-    #current_state = readState(ser)
-    #return (current_state)
 
-def GoEl(current_state, elGe=None):
+def GoEl(elGe=None):
     if elGe == None:
         elGe = input("El: ")
         elGe = float(elGe)
-    d_elge = elGe - float(current_state['elDeg'][0])
+    d_elge = elGe - float(mrtstate.state['elDeg'][0])
     print ('d_el: ',d_elge)
     if (elGe >= -eloff and elGe <= 120.):
-        current_state = StdCmd(ser,ELEVATION)
-        current_state = StdCmd(ser,ENABLE)
+        StdCmd(ser,ELEVATION)
+        StdCmd(ser,ENABLE)
+        print ('Elevation move starting')
+        PrintState()
         if d_elge < 0:
-            # If moving to a less positive azimuth, go CCW
-            current_state = StdCmd(ser,REVERSE)
-            print ('Starting from')
-            PrintState()
-            d,current_state = Scan(ser,str(np.abs(d_elge)))
-            current_state = readState(ser)
-            print ('Ending at')
-            PrintState()
+            StdCmd(ser,REVERSE)   
         else:
-            # If moving to a more positive azimuth, go CW
-            current_state = StdCmd(ser,ELEVATION)
-            current_state = StdCmd(ser,FORWARD)
-            print ('Starting from')
-            PrintState()
-            d,current_state = Scan(ser,str(np.abs(d_elge)))
-            current_state = readState(ser)
-            print ('Ending at')
-            PrintState()
+            StdCmd(ser,FORWARD)
+        print(str(np.abs(d_elge)))
+        Scan(ser,np.abs(d_elge))
+        print('Elevation move ended at')
+        PrintState()
+        print ('Final state')
+        PrintState()
     else:
-        print ('Requested az out of bounds')
+        print ('Requested elevation out of bounds')
         elGe=None
-    current_state = readState(ser)
-    return (current_state)
     
-def RasterMap(current_state):
+def RasterMap():
     """ Super hard coded to get something going.  Start where you are, and
     make a 20 x 20 degree map """
 
@@ -429,7 +322,15 @@ def RasterMap(current_state):
     elG = float(elG)
     azM = azG-10.
     elM = elG-10.
-    GoTo(current_state,azG=azM,elG=elM)
+    GoTo(azG=azM,elG=elM)
+    #DIMX = input("X length: ")
+    #DIMX = float(DIMX)
+    #DIMY = input("Y length: ")
+    #DIMY = float(DIMY)
+    DIM = 20
+    DIMF = float(DIM)
+    ONE = 1
+    ONEF = float(ONE)
     
     #plt.figure(1)
     #plt.clf()
@@ -439,28 +340,28 @@ def RasterMap(current_state):
     pwr = np.array([])
     for i in np.arange(10):
         print (i,'of 10')
-        cs = StdCmd(ser,AZIMUTH)
-        cs = StdCmd(ser,REVERSE)
-        d,cs = Scan(ser,'20')
+        StdCmd(ser,AZIMUTH)
+        StdCmd(ser,REVERSE)
+        Scan(ser,DIMF)
         #plt.subplot(10,1,i+1)
         #plt.plot(d['azDeg'],d['pwr'])
-        az = np.append(az,d['azDeg'])
-        el = np.append(el,d['elDeg'])
-        pwr = np.append(pwr,d['pwr'])
-        cs = StdCmd(ser,ELEVATION)
-        cs = StdCmd(ser,REVERSE)
-        d,cs= Scan(ser,'1')
-        cs = StdCmd(ser,AZIMUTH)
-        cs = StdCmd(ser,FORWARD)
-        d,cs = Scan(ser,'20')
+        az = np.append(az,['azDeg'])
+        el = np.append(el,['elDeg'])
+        pwr = np.append(pwr,['pwr'])
+        StdCmd(ser,ELEVATION)
+        StdCmd(ser,REVERSE)
+        Scan(ser,ONEF)
+        StdCmd(ser,AZIMUTH)
+        StdCmd(ser,FORWARD)
+        Scan(ser,DIMF)
         #plt.subplot(10,1,i+1)
         #plt.plot(d['azDeg'],d['pwr'])
-        az = np.append(az,d['azDeg'])
-        el = np.append(el,d['elDeg'])
-        pwr = np.append(pwr,d['pwr'])
-        cs = StdCmd(ser,ELEVATION)
-        cs = StdCmd(ser,REVERSE)
-        d,cs = Scan(ser,'1')
+        az = np.append(az,['azDeg'])
+        el = np.append(el,['elDeg'])
+        pwr = np.append(pwr,['pwr'])
+        StdCmd(ser,ELEVATION)
+        StdCmd(ser,REVERSE)
+        Scan(ser,ONEF)
     
     #plt.show()
     plt.figure(2,figsize=(8,8))
@@ -492,10 +393,10 @@ def PrintMenu():
     """ Provide the user the available commands """
     return
 
-def ScanSouthSky(current_state):
+def ScanSouthSky():
     
-    Saz = float(current_state['azDeg'][0])
-    Sel = float(current_state['elDeg'][0])
+    Saz = float(mrtstate.state['azDeg'][0])
+    Sel = float(mrtstate.state['elDeg'][0])
     
     if ((Saz >=89.5 and Saz <= 90.5) and (Sel >=79.5 and Sel<= 80.5)):
             az = np.array([])
@@ -503,27 +404,27 @@ def ScanSouthSky(current_state):
             pwr = np.array([])
             for i in np.arange(45):
                 print (i,'of 50')
-                cs = StdCmd(ser,AZIMUTH)
-                cs = StdCmd(ser,REVERSE)
+                StdCmd(ser,AZIMUTH)
+                StdCmd(ser,REVERSE)
                 d,cs = Scan(ser,'180')
                 #plt.subplot(10,1,i+1)
                 #plt.plot(d['azDeg'],d['pwr'])
                 az = np.append(az,d['azDeg'])
                 el = np.append(el,d['elDeg'])
                 pwr = np.append(pwr,d['pwr'])
-                cs = StdCmd(ser,ELEVATION)
-                cs = StdCmd(ser,REVERSE)
+                StdCmd(ser,ELEVATION)
+                StdCmd(ser,REVERSE)
                 d,cs= Scan(ser,'1')
-                cs = StdCmd(ser,AZIMUTH)
-                cs = StdCmd(ser,FORWARD)
+                StdCmd(ser,AZIMUTH)
+                StdCmd(ser,FORWARD)
                 d,cs = Scan(ser,'180')
                 #plt.subplot(10,1,i+1)
                 #plt.plot(d['azDeg'],d['pwr'])
                 az = np.append(az,d['azDeg'])
                 el = np.append(el,d['elDeg'])
                 pwr = np.append(pwr,d['pwr'])
-                cs = StdCmd(ser,ELEVATION)
-                cs = StdCmd(ser,REVERSE)
+                StdCmd(ser,ELEVATION)
+                StdCmd(ser,REVERSE)
                 d,cs = Scan(ser,'1')
     
             #plt.show()
