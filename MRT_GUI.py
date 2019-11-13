e551f81b6b32b1aa055a9b952a6231bb21ff59b2
Brian Shen <brianxshen@users.noreply.github.com>
2019-11-12 19:36:19 -0500

    Added Sample Scan and Map Plots

--- MRT_GUI.py
+++ MRT_GUI.py
@@ -2,10 +2,13 @@ import os
 import sys
 import tkinter as tk
 from tkinter import ttk
+
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import style
 from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.ticker import MaxNLocator
 
 debug = False
 
@@ -21,7 +24,10 @@ G91 - Use Relative Angles
 G92 A### E### - Set Current Position
 M18 - Disable Motors
 M84 S### - Disable after S seconds of inactivity
+M105 - Report Current Readings
 M114 - Report Current Position
+M350 Ann Enn - Microstepping Mode (Full - 1/16) nn:(1, 2, 4, 8, 16)
+N#### - Line Number of G-Code Sent
 '''
 
 
@@ -33,12 +39,12 @@ class mrtGUI(tk.Tk):
         self.geometry('1280x800')
         self.title('Mini Radio Telescope')
 
-        mainTabList = (tabControl, tabScan, tabMap, tabTerminal)
-        mainTabNames = ['Control', 'Scan', 'Map', 'Terminal']
+        mainTabList = (tabControl, tabScan, tabMap, tabTerminal, tabViewer)
+        mainTabNames = ['Control', 'Scan', 'Map', 'Terminal', 'Viewer']
         infoTabList = (tabConnection, tabState)
         infoTabNames = ['Connection', 'State']
-        # filesTabList = (tabFiles)
-        # filesTabNames = ['Files']
+        filesTabList = (tabGraph, tabRawData)
+        filesTabNames = ['Graphs', 'Raw Data']
 
         notebookMain = ttk.Notebook(self)
         notebookMain.pack(expand=1, fill='both', side='right')
@@ -55,9 +61,9 @@ class mrtGUI(tk.Tk):
             tab = t(notebookInfo)
             notebookInfo.add(tab, text=infoTabNames[infoTabList.index(t)])
 
-        # for t in filesTabList:
-        #     tab = t(notebookFiles)
-        #     notebookFiles.add(tab, text=filesTabNames[filesTabList.index(t)])
+        for t in filesTabList:
+            tab = t(notebookFiles)
+            notebookFiles.add(tab, text=filesTabNames[filesTabList.index(t)])
 
 
 class tabConnection(ttk.Frame):
@@ -129,7 +135,20 @@ class tabScan(ttk.Frame):
         # sep = ttk.Separator(frameConnection, orient='horizontal')
         # sep.grid(row=2, column=0, columnspan=2, sticky='we')
 
-        canvasScan = FigureCanvasTkAgg(figScan, self)
+        ''' Graph '''
+        x = np.arange(0.0, 2.0, 0.01)
+        y = 1 + np.sin(2 * np.pi * x)
+
+        figureScan = plt.figure()
+
+        plotScan = figureScan.add_subplot(111)
+        plotScan.plot(x, y, '.-')
+
+        plotScan.set_title('Yeet!')
+        plotScan.set_xlabel('Azimuth (°)')
+        plotScan.set_ylabel('Power (μW)')
+
+        canvasScan = FigureCanvasTkAgg(figureScan, self)
         canvasScan.draw()
         canvasScan.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True, anchor=tk.S)
 
@@ -142,20 +161,100 @@ class tabMap(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
 
+        frameMapControl = ttk.Frame(self)
+        frameMapControl.pack(side='bottom')
+
+        optionListScanDirection = ['Up', 'Down', 'Clockwise', 'Counterclockwise']
+
+        ''' Variables '''
+        varMapCenterAzimuth = tk.StringVar()
+        varMapCenterElevation = tk.StringVar()
+        varMapHeight = tk.StringVar()
+        varMapWidth = tk.StringVar()
+
+        ''' Label '''
+        labelMapCenterAzimuth = ttk.Label(frameMapControl, text='Center Azimuth:').grid(row=0, column=0, sticky='nsew')
+        labelMapCenterElevation = ttk.Label(frameMapControl, text='Center Elevation:').grid(row=1, column=0, sticky='nsew')
+        labelMapDirection = ttk.Label(frameMapControl, text='Height:').grid(row=0, column=2, sticky='nsew')
+        labelMapAmount = ttk.Label(frameMapControl, text='Width:').grid(row=1, column=2, sticky='nsew')
+
+        ''' Entry '''
+        entryMapStartAzimuth = ttk.Entry(frameMapControl, textvariable=varMapCenterAzimuth).grid(row=0, column=1)
+        entryMapStartElevation = ttk.Entry(frameMapControl, textvariable=varMapCenterElevation).grid(row=1, column=1)
+        entryMapHeight = ttk.Entry(frameMapControl, textvariable=varMapHeight).grid(row=0, column=3)
+        entryMapWidth = ttk.Entry(frameMapControl, textvariable=varMapWidth).grid(row=1, column=3)
+
+        ''' Button '''
+        buttonMap = ttk.Button(frameMapControl, text='Map').grid(row=0, column=4, rowspan=2)
+
+        ''' Other '''
+        # progressBar = ttk.Progressbar(frameScanControl, orient='horizontal', length=100, mode='determinate').grid(row=2, column=0, columnspan=5, sticky='nsew')
+        # sep = ttk.Separator(frameConnection, orient='horizontal')
+        # sep.grid(row=2, column=0, columnspan=2, sticky='we')
+
+        ''' Graph '''
+        # make these smaller to increase the resolution
+        dx, dy = 0.05, 0.05
+
+        # generate 2 2d grids for the x & y bounds
+        y, x = np.mgrid[slice(1, 5 + dy, dy), slice(1, 5 + dx, dx)]
+
+        z = np.sin(x) ** 10 + np.cos(10 + y * x) * np.cos(x)
+
+        z = z[:-1, :-1]
+
+        # Colorbar Scaling
+        levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
+
+        figureMap = plt.figure(2)
+        plotMap = figureMap.add_subplot(111)
+
+        cf = plotMap.contourf(x[:-1, :-1] + dx/2., y[:-1, :-1] + dy/2., z, levels=levels, cmap=plt.cm.jet)
+
+        cb = figureMap.colorbar(cf, ax=plotMap)
+
+        plotMap.set_title('Yoink!')
+
+        plotMap.set_xlabel('Azimuth (°)')
+        plotMap.set_ylabel('Elevation (°)')
+
+        cb.minorticks_on()
+        cb.set_label('Power (μW)')
+
+        # Tkinter Matplotlib Graphing Code
+        canvasMap = FigureCanvasTkAgg(figureMap, self)
+        canvasMap.draw()
+        canvasMap.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True, anchor=tk.S)
+
+        toolbarMap = NavigationToolbar2Tk(canvasMap, self)
+        toolbarMap.update()
+        canvasMap.tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True, anchor=tk.S)
+
 
 class tabTerminal(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
 
 
-class tabFiles(ttk.Frame):
+class tabViewer(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent)
+
+
+class tabGraph(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent)
+
+
+class tabRawData(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
 
 
 # # Figure # #
-figScan = plt.figure()  # figsize=(5, 4), dpi=100
-a = figScan.add_subplot(111)
+  # figsize=(5, 4), dpi=100
+
+# a = figScan.add_subplot(111)
 style.use('ggplot')
 
 
